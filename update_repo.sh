f47eaae26eed0c5442fdeca59caad556f354db1d
arnaldo <arnaldoavj@insper.edu.br>
2020-10-16 14:07:54 -0300

    update_repo.sh

---
+++ update_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Atualizando o hostname..." >> ~/update_repo.txt
+echo "atualizando repositorio git bumper."
+cd /home/ubuntu/catkin_ws/src/bumper
+git pull
+echo "atualizando repositorio git servo_arm."
+cd /home/ubuntu/catkin_ws/src/servo_arm
+git pull
+echo "atualizando repositorio git servo_camera."
+cd /home/ubuntu/catkin_ws/src/servo_camera
+git pull
+echo "Repositórios atualizados."
+date >> ~/update_repo.txt

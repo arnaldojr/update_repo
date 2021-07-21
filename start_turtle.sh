6abda4a768dcb197a1d228747b55418586d2964b
Licia Sales <licia.sales@hotmail.com>
2021-07-21 18:37:22 -0300

    adicionando telinha

--- start_turtle.sh
+++ start_turtle.sh
@@ -4,21 +4,21 @@ git pull
 sleep 3
 screen -dmS ROBO bash
 screen -S ROBO -X screen -t UPDATE_REPO bash -ic "bash /home/ubuntu/update_repo/update_repo.sh"
-sleep 3
+sleep 1
 screen -S ROBO -X screen -t ROSCORE bash -ic "roscore"
 sleep 5
 screen -S ROBO -X screen -t LIDAR bash -ic "roslaunch turtlebot3_bringup turtlebot3_robot.launch"
 sleep 5
 screen -S ROBO -X screen -t SERVOCAM bash -ic "rosrun servo_camera driver_node"
-sleep 3
+sleep 1
 screen -S ROBO -X screen -t SERVOARM bash -ic "rosrun servo_arm arm_node"
-sleep 3
+sleep 1
 screen -S ROBO -X screen -t BUMPER bash -ic "rosrun bumper bumper"
-sleep 3
+sleep 1
 screen -S ROBO -X screen -t CAMERA bash -ic "python3 /home/ubuntu/camera.py"
-sleep 5
-#screen -S ROBO -X screen -t TELINHA bash -ic "/home/ubuntu/net/control.py"
-#sleep 2
+sleep 1
+screen -S ROBO -X screen -t TELINHA bash -ic "python3 /home/ubuntu/catkin_ws/src/update_repo/menu.py"
+sleep 1
 screen -S ROBO -X screen -t BEEP bash -ic "rostopic pub -1 /sound turtlebot3_msgs/Sound 'value: 1'"
 
 
